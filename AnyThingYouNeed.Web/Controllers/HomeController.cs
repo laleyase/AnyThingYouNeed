1a7fdb9439a5cf694869fc62d9ec4570af39f9b3
yasemin lale <yasemingulbuz@hotmail.com>
2024-04-05 22:52:53 +0300

    controller

--- AnyThingYouNeed.Web/Controllers/HomeController.cs
+++ AnyThingYouNeed.Web/Controllers/HomeController.cs
@@ -1,16 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AnyThingYouNeed.Bussiness.Abstract;
+using AnyThingYouNeed.Entities.Concrate;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AnyThingYouNeed.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IRequestService _requestService;
+        public HomeController(IRequestService requestService)
+        {
+            _requestService = requestService;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult MakeRequest()
+        public JsonResult MakeRequest(Request model)
         {
-            return View();
+
+            var addedRequest = _requestService.AddRequest(model);
+            if (addedRequest.Success == true)
+            {
+                return Json(addedRequest.Message);
+            }
+            else
+            {
+                return Json(addedRequest.Message);
+            }
+
         }
+
+
     }
 }
---
+++ AnyThingYouNeed.Web/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using AnyThingYouNeed.Bussiness.Abstract;
+using AnyThingYouNeed.Bussiness.Concrate.RequestModel;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExperienceIst.Wep.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Login(LoginRequestModel loginRequest)
+        {
+            var result = _userService.Login(loginRequest);
+            if (result.Success == true)
+            {
+                //HttpContext.Response.Headers.TryAdd("Authorization", result.Token.JWT);
+                Response.Cookies.Append("JWTToken", result.Token.JWT, new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict,
+                    MaxAge = TimeSpan.FromMinutes(20)
+                });
+                return Json(result.Message);
+            }
+            else
+            {
+                return Json(result.Message);
+            }
+        }
+
+    }
+}
